284aeda42b4d8b617db1fc36f6a18612a43e889e
Ali Rashidi <alirashidi127@gmail.com>
2020-11-27 14:52:07 +0330

    Added RandowmWords stateful widget to render pairs of words

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'Startup Namer',
       home: Scaffold(
@@ -14,9 +13,22 @@ class MyApp extends StatelessWidget {
           title: Text('Startup Namer'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
